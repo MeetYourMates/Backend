be5ef76d69d17cba57984647c3bf55f786769f2f
Jose-Verdu-Diaz <pepv@hotmail.es>
2020-11-25 19:32:37 +0100

    Modelo Team editado

--- backend/src/models/team.ts
+++ backend/src/models/team.ts
@@ -1,5 +1,7 @@
 import mongoose, { Schema, Document} from 'mongoose';
 import Chat, { IChat } from './chat';
+import Task, { ITask } from './task';
+import Meeting, { IMeeting } from './meeting';
 //Modelo de objeto que se guarda en la BBDD de MongoDB
 const teamSchema = new Schema({
     name: {
@@ -8,9 +10,17 @@ const teamSchema = new Schema({
     availability: {
         type: Number
     },
-    chat: {
+    chats: {
         type: Schema.Types.ObjectId,
         ref: Chat
+    },
+    tasks: {
+        type: Schema.Types.ObjectId,
+        ref: Task
+    },
+    meetings: {
+        type: Schema.Types.ObjectId,
+        ref: Meeting
     }
 });
 
@@ -19,6 +29,8 @@ export interface ITeam extends Document {
     name: String;
     availability: Number;
     chat: IChat['_id'];
+    tasks: ITask['_id'];
+    meetings: IMeeting['_id'];
 }
 
 //Exportamos modelo para poder usarlo 
