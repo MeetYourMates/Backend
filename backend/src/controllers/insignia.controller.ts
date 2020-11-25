c73340ed1e2ea5a6ba398cb0b93f6bf43a112ade
EgidoPol <61285510+EgidoPol@users.noreply.github.com>
2020-11-25 18:49:17 +0100

    Controllers

--- backend/src/controllers/insignia.controller.ts
+++ backend/src/controllers/insignia.controller.ts
@@ -1,37 +1,37 @@
 import {Request, Response} from 'express';
-import Trophy from '../models/trophy';
+import Insignia from '../models/insignia';
 
-const getTrophies = async (req: Request, res: Response) => {
+const getInsignia = async (req: Request, res: Response) => {
     //El await hace que la siguiente linea no se ejecute
     //hasta que el resultado no se haya obtenido
     try{
-        const results = await Trophy.find({});
+        const results = await Insignia.find({});
         return res.status(200).json(results);
     } catch (err) {
         return res.status(404).json(err);
     }
 }
 
-const getTrophy = async (req: Request, res: Response) => {
+const getInsignias = async (req: Request, res: Response) => {
     try{
-        const results = await Trophy.find({"user": {"email":req.body.email,"token":req.body.token}});
+        const results = await Insignia.find({"user": {"email":req.body.email,"token":req.body.token}});
         return res.status(200).json(results);
     } catch (err) {
         return res.status(404).json(err);
     }
 }
-const newTrophy = async (req: Request, res: Response) => {
-    const trophy = new Trophy({
+const newInsignia = async (req: Request, res: Response) => {
+    const insignia = new Insignia({
     
         "title": req.body.title,
         "difficulty": req.body.difficulty,
         "professor": req.body.professor,
         "logo": req.body.logo
     });
-    trophy.save().then((data) => {
+    insignia.save().then((data) => {
         return res.status(201).json(data);
     }).catch((err) => {
         return res.status(500).json(err);
     })
 }
-export default {getTrophies, getTrophy,newTrophy};
+export default {getInsignias, getInsignia,newInsignia};
