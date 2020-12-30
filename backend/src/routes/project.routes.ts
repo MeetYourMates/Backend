432653d4662756eadee6ded785b6b479aed57476
Krunal <krunalmiracle@gmail.com>
2020-12-30 15:33:12 +0100

    Update project.routes.ts

--- backend/src/routes/project.routes.ts
+++ backend/src/routes/project.routes.ts
@@ -1,8 +1,5 @@
 import { Router } from 'express';
 import projectController from "../controllers/project.controller";
-//! Not Uploaded to GitHub!! 
 const router = Router();
 router.post('/add', projectController.addProject);
-//! Not getting Uploaded 
-//! Not getting Uploaded 
 export default router;
