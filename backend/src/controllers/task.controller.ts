80509944cda0a4bf53b8c33fa1a8cfb2071d5e44
Krunal <krunalmiracle@gmail.com>
2021-01-26 17:16:41 +0100

    Fixed Task

--- backend/src/controllers/task.controller.ts
+++ backend/src/controllers/task.controller.ts
@@ -7,10 +7,10 @@ const addTask = async(req: Request, res: Response) =>{
     //Display request
     console.log(req.body);
     if(req.body==null){
-        res.status(400).send({message: 'Bad Request'});
+        return res.status(400).send({message: 'Bad Request'});
     }
-    if(req.body.subjectId==null){
-        res.status(400).send({message: 'Bad Request'});
+    if(req.body.teamId==null){
+        return res.status(400).send({message: 'Bad Request'});
     }
     //Set variables for the data found in the request body
     let teamId:string = req.body.teamId;
@@ -25,40 +25,29 @@ const addTask = async(req: Request, res: Response) =>{
     });
     task.save().then((data) => {
         //Add Task to Team
-        Team.findOne({_id: [teamId]}).then(team => {
-            //No error and we got a result
-            console.log("Adding Task to Team: ");
-            console.log([team]);
-            if(team!=null){
-                //@ts-ignore
-                Team.updateOne({"_id":team._id}, {$addToSet: {tasks: data!._id}}).then(result => {
-                    if (result.nModified> 0) {
-                        res.status(201).send({message: 'Task added succesfully!'});
-                    }else{
-                        res.status(409).send({message: 'Task was already added!'});
-                    }
-                });
+        // @ts-ignore
+        Team.updateOne({"_id":req.body.teamId}, {$addToSet: {tasks: data!._id}}).then(result => {
+            if (result.nModified> 0) {
+               return res.status(201).send({message: 'Task added succesfully!'});
             }else{
-                //No Course Found
-                res.status(400).send({message: 'No Subject in Database'});
+                return res.status(409).send({message: 'Task was already added!'});
             }
-
+        });
         }).catch((err) => {
             console.log("error ", err);
-            res.status(500).json({message: 'Server Error!'});
+            return res.status(500).json({message: 'Server Error!'});
         });
-    });
 
 }
 const getTasksFromTeam = async (req: Request, res: Response) => {
     //El await hace que la siguiente linea no se ejecute
     //hasta que el resultado no se haya obtenido
     try{
-        let results = await Team.find({_id:req.params.id}).select('tasks').populate({
+        let results = await Team.findOne({_id:req.params.id}).select('tasks').populate({
             path: 'tasks',
         }).lean();
         //"Limpia" la encapsulación del json
-        results = results[0]['tasks'];
+        //results = results['tasks'];
         console.log(results);
         return res.status(200).json(results);
 
